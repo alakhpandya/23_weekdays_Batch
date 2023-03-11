4e91c6e82f0acee8d251c3932a43f42d81c4e183
alakhpandya <alakhpandya@gmail.com>
2023-03-11 20:02:08 +0530

    Create mar11.py

---
+++ mar11.py
@@ -0,0 +1,73 @@
+# Datatypes in Python: Numeric / Non-numeric
+
+'''
+# Numeric: int
+a = 10
+print("a =", a)
+print(type(a))
+
+n = 987245435625743078657098710894237098714504657104832789435709867904375981073987405987676013471947128973470598670329875109823740985709173497
+print("n =", n)
+print(type(n))
+
+
+# Numeric: float
+b = 5.5
+print(b)
+print(type(b))
+
+
+# Numeric: complex
+"""
+sqare root of 9 = 3 or -3
+because (-3) x (-3) = 9
+
+square root of -16 = not real
+beacause (a) x (a) = -16
+
+square root of -16 = square root of (16 x -1)
+		   = (square root of 16) x (square root of -1)
+		   = 4j
+
+complex nos: 3j, 8 - 7j, 5 + 9j, 2.3 - 3.4j
+"""
+c = 8 - 7j
+print(c)
+print(type(c))
+
+d = 5 + 9j
+print(c + d)
+'''
+
+# Non-Numeric: str
+"""
+s1 = "Alakh"
+s2 = 'P'
+print(s1)
+print(type(s1))
+
+print(s2)
+print(type(s2))
+"""
+
+# Taking input from user:
+"""
+print("Enter a number:")
+a = input()
+print("a =", a)
+print("Enter second number:")
+b = input()
+print("b =", b)
+
+
+a = input("Enter a number: ")
+b = input("Enter second number:")
+print("a =", a, "b =", b)
+"""
+
+print("Enter two numbers:")
+a = input()
+b = input()
+print("a =", a, "\nb =", b)
+
+# Next Class: Adding these inputs
