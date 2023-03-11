169e4eacec88da066b6bc04a5f180bd141fdabe3
alakhpandya <alakhpandya@gmail.com>
2023-03-11 19:19:11 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,43 @@
+print("Hello World!")
+print('How are you?')		# Python print() statement ends with '\n' by default
+
+# Single line comments / in-line comments
+# We can replace pair of double quotes with pair of sigle quotes across Python.
+
+"""
+Multi
+Line
+Comments
+"""
+'''
+Another way
+to comment
+multiple lines
+'''
+print("Alakh Pandya",        "9876543210")	# print() gives space between two arguments
+print("Alakh Pandya"    '9876543210')
+# Not valid - print("Alakh Pandya'    "9876543210")
+
+# Programming Exercise
+"""
+print the following messages on the terminal as they appear:
+1) Alpha said Beta "Gamma is teasing Delta."
+2) Alpha said Beta "Gamma is teasing Delta but Gamma says he didn't tease Delta. Now what to do?"
+3) The location of Python.exe is: 'C:\new folder\temp\Python'
+"""
+
+# Escape Sequence Characters
+"""
+\	Escape sequence character
+\n	new line character
+\t	tab character
+"""
+print("Alpha said Beta \"Gamma is teasing Delta.\"")
+print("The location of Python.exe is: 'C:\\new folder\\temp\\Python'")
+
+# HW:
+"""
+print the following message on the terminal as they appear:
+
+In Python, we use '\t' to give "tab". But if we want to 'escape' it, we need to type '\\t'.
+"""
---
+++ mar10.py
@@ -0,0 +1,24 @@
+# print("Hello!", end = "")
+# print("Hello!", end = "\t")
+# print("Hello!", end = "Python")
+# print("Hello!", end = " ")
+# print("How are you?")
+
+"""
+\b	backspace
+"""
+print("Hello World!\b\b\brior!")
+print("Hello World!\b\b\b\b\b\bYou")
+
+# Creating variables
+"""
+int r = 10(10);
+float h = 171.5;
+char n = 'A';
+"""
+r = 17
+# print("r")
+print(r)
+
+n = 987245435625743078657098710894237098714504657104832789435709867904375981073987405987676013471947128973470598670329875109823740985709173497
+print(n)
