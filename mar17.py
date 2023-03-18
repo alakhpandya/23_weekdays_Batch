62e2706688ee1e9b34ae55e1f3558104911eee1c
alakhpandya <alakhpandya@gmail.com>
2023-03-18 20:00:00 +0530

    mar18 commit

---
+++ mar17.py
@@ -0,0 +1,22 @@
+a = int(input("Enter two integers:\n"))
+b = float(input())
+# print(type(a))
+# print(type(b))
+# a = int(a)
+# print("Sum =", int(a) + float(b))
+print(type(a))
+print(type(b))
+# Concatenation = joining two strings
+# convert to other data type = type casting
+
+print(a + b)
+
+# HW:
+"""
+Ask five integers from user. Store them in variables named a, b, c, d & e. Calculate their average and call it avg. Print the output exactly as below:
+
+for example, inputs are: 3, 4, 5, 6 & 7
+output:
+
+The Average of 3, 4, 5, 6 and 7 is: 5.0
+"""
---
+++ mar18.py
@@ -0,0 +1,28 @@
+# f-string: formatted string
+"""
+print("Enter five integers:")
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+avg = (a + b + c + d + e)/5
+
+print(f"The Average of {a}, {b}, {c}, {d} and {e} is: {avg}")
+"""
+
+# string: Ordered & Immutable collection of characters
+a = 	   "Today is Saturday and I am very excited for tomorrow!"
+# index no: 0123456789...........................................
+print(a)
+print(type(a))
+"""
+1st - latest iPhone 		- Divyanshi	Kavyan
+2nd - Dubai Trip 		- Jesal		Ritu
+3rd - Local Trip 		- Ritu		Jesal
+4th - Cash prize of Rs.10,000 	- Kavyan	Divyanshi
+"""
+print(a[6])
+
+
+
