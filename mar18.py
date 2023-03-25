f79160837874ca571443c7c66b30c630063511df
alakhpandya <alakhpandya@gmail.com>
2023-03-25 21:05:51 +0530

    mar24

--- mar18.py
+++ mar18.py
@@ -12,17 +12,27 @@ print(f"The Average of {a}, {b}, {c}, {d} and {e} is: {avg}")
 """
 
 # string: Ordered & Immutable collection of characters
+
 a = 	   "Today is Saturday and I am very excited for tomorrow!"
 # index no: 0123456789...........................................
 print(a)
 print(type(a))
-"""
+'''
 1st - latest iPhone 		- Divyanshi	Kavyan
 2nd - Dubai Trip 		- Jesal		Ritu
 3rd - Local Trip 		- Ritu		Jesal
 4th - Cash prize of Rs.10,000 	- Kavyan	Divyanshi
-"""
+'''
+c = int(input("Enter c: "))
+b = int(input("Enter b: "))
+print("c + b =", c+b)
 print(a[6])
+# name = "Yash"
+print(len(a))
+# Accessing through index numbers
+print(a[52])
+print(a[2])
+# print(a[354])
 
-
-
+# slicing
+print(a[2 : 4])		# first one is included, last one is excluded
---
+++ mar24.py
@@ -0,0 +1,51 @@
+s1 = 	   "Today is Friday and I am not really very excited for tomorrow!"
+# index no: 0123456789....................................................
+#-ve index: .....................................................987654321
+"""
+s2 = s1[3 : 30]
+print(s1)	# slicing never changes the original collection
+print(s2)
+print(s1[7 : 354])
+
+myString = input("Enter your name with title(Dr./Mr./Ms.): ")	# Ms. Ritu
+
+myString_length = len(myString)
+updated_string = myString[4 : myString_length]
+print(updated_string)
+
+updated_string = myString[4 : ]
+print(updated_string)
+new_string = myString[ : 10]
+"""
+print(s1[ : ])
+print(s1[0])
+
+print(len(s1))
+print(s1[-53])
+print(s1[-53 : -4])
+print(s1[-5 : -40])
+print("End of program...")
+
+print(s1[4 : -5])
+print(s1[-6 : ])
+print(s1[4 : 60 : 3])
+
+s1 = 	   "Today is Friday and I am not really very excited for tomorrow!"
+# index no: 0123456789....................................................
+#-ve index: .....................................................987654321
+
+print(s1[3 : 20 : 1])
+print(s1[3 : 20 : ])
+
+# defaults: str[0 : len(str) : 1]
+print(s1[ 30 : 3 : -1])
+print(s1[ : : -1])
+print(s1[ : : -3])
+
+# Next Class: String Methods
+
+
+
+
+
+
