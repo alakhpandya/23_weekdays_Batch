bf1d16f0b8dca908d253a84accaacb554430fcae
alakhpandya <alakhpandya@gmail.com>
2023-04-03 14:27:15 +0530

    apr3 commit

---
+++ apr1.py
@@ -0,0 +1,27 @@
+s1 = "Hello"
+s2 = "World!"
+s3 = "I am going to play outside if it is NOT raining."
+
+s4 = 5
+s5 = 7
+
+# print(s4 + s5)
+# Magical Methods/Dunder Methods
+# print(s1.__add__(s2))
+# print(s4.__add__(s5))
+
+# s3.casefold() same as s3.lower()
+# print(s3.casefold())
+
+
+# Allignment Related Methods
+"""
+s6 = s2.center(100)
+s6 = s2.center(100, "-")
+print(len(s6))
+print(s6)
+
+print(s2.rjust(120, "$"))
+
+print(s2.ljust(110, "~"))
+"""
---
+++ mar31.py
@@ -0,0 +1,31 @@
+# strings: Ordered & Immutable collections of characters
+
+# string methods
+
+s1 = "Welcome all the NEW STUDENTS on board!"
+s3 = "abcd"
+# print(s1)
+# functions
+# print(len(s1))
+# print(type(s1))
+a = 10
+# print(type(a))
+b = 11.1
+# print(type(b))
+
+
+# Methods - capitalize
+
+# Case-related methods
+print(s1.capitalize())
+s2 = s1.capitalize()
+print(s2)
+# print(s1)
+print(s3.capitalize())
+print(s1.upper())
+print(s1.lower())
+print(s1.title())
+print(s1.swapcase())
+
+# Next Class: Magical Methods/Dunder Methods
+# s1.__add__
