16d7a1a6b2d5b7130f3fc925313493ae3c012de8
alakhpandya <alakhpandya@gmail.com>
2023-04-08 21:06:05 +0530

    Create apr7.py

---
+++ apr7.py
@@ -0,0 +1,70 @@
+s1 = "I will go to sleep in balcony if it is NOT raining."
+
+# print(s1.count('o'))
+# print(s1.count('i', 5))
+# print(s1.count('i', 5, 20))
+# print(s1.count('sleep', 5, 20))
+
+# print(s1.encode("utf-16"))
+# print(s1.endswith("."))
+# print(s1.endswith("?"))
+
+# Boolean Data Type: 
+# print(type(True))
+# print(type(False))
+
+# print(True)
+# print(False)
+# False: False, 0, ""
+
+# print(s1.endswith('raining.'))
+# print(s1.endswith('e', 5, 25))
+# print(s1.startswith('A'))
+# print(s1.startswith('l', 5, 25))
+
+
+
+"""
+Subject                 Test1   Test2
+C                       22      24
+Chemistry               19      22
+C++                     20      23
+Advanced Java           21      21
+Maths                   21      25
+AI & Machine Learning   18      23
+Python                  25      25
+"""
+# print("Subject\tTest1".expandtabs(25) + "\tTest2")
+# print("C\t22".expandtabs(25) + "\t24")
+# print("AI & Machine Learning\t18".expandtabs(25) + "\t23")
+
+
+# print(s1.find("N"))
+# print(s1.find("i"))
+# print(s1.find("i", 20))
+# print(s1.find("i", 30, 5000))
+# print(s1.find("i", 20, 29))
+
+# print(s1.rfind("i"))
+# print(s1.rfind("i", 20, 40))
+# print(s1.rfind("is", 20, 40))
+
+# print(s1.find('b'))
+# print(s1.rfind('b'))
+
+# print(s1.find('i', -30, -10))
+# print(s1.find('in', 10, 30))
+# print(s1.rfind('in'))
+
+# print(s1.count('in'))
+
+# print(s1.index('i', 20, 40))
+# print(s1.find('z'))
+# print(s1.index('z'))
+# print(s1[15])
+
+# print(s1.rindex('in'))
+
+"""
+islower()?  -> Yes/No   -> True/False
+"""
