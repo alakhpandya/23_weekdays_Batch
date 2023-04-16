90d029a2c13ecf6755b9ee9abe3addbd45d0e96c
alakhpandya <alakhpandya@gmail.com>
2023-04-16 09:37:54 +0530

    Create apr14.py

---
+++ apr14.py
@@ -0,0 +1,49 @@
+# print(5 < 10)
+# print(5 > 10)
+
+"""
+False: 0, "", 0.0, False, set(), [], (), {}
+True: Anything that is not listed above
+More to come in operator's lecture
+"""
+
+s1 = "I will go to sleep in balcony if it is NOT raining."
+s2 = "i am in lower case."
+s3 = "I AM IN UPPER CASE"
+s4 = "I Am In Title Case"
+
+s5 = "AlakhPandya"
+# print(s2.islower())
+# print(s1.islower())
+# print(s1.isupper())
+# print(s4.istitle())
+
+# print(s1.isalpha())
+# print(s3.isalpha())
+# print(s5.isalpha())
+
+s6 = "9876543210"
+# print(s6.isnumeric())
+# print(s6.isdecimal())
+# print(s6.isdigit())
+
+s7 = "AlakhPandya9876543210"
+# print(s7.isalnum())
+# print(s5.isalnum())
+# print(s6.isalnum())
+
+
+# print(s1.isascii())
+s8 = "Hello₹World"
+# print(len(s8))
+# print(s8.isascii())
+
+s9 = "forifelsewhile"
+# print(s9.isidentifier())
+
+s10 = "Hello\nWorld"
+# print(s1.isprintable())
+# print(s10.isprintable())
+
+s11 = "             \n  \n\n         \t\t\t           \n\t      "
+# print(s11.isspace())
